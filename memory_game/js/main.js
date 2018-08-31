468389c155bca349ab2cca1d6836b4f7e9f1d86d
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-08-31 14:19:30 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,27 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+  rank: 'queen',
+  suit: 'hearts',
+  cardImg: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImg: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImg: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImg: "images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -15,8 +36,10 @@ var checkForMatch = function() {
 };
 
 var flipCard = function(cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log(cards[cardId].cardImg);
+  console.log(cards[cardId].suit);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 };
 flipCard(0);
